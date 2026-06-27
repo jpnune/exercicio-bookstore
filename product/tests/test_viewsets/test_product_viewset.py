dc46a27139632af8a71eae1ed9d34d4b077b277e
Joao Paulo Nunes <jpnune@gmail.com>
2026-06-27 15:32:03 -0300

    feat: ajusta docker-compose, env, testes de produto e adiciona workflows de CI

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -2,7 +2,8 @@ import json
 
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
 from product.factories import CategoryFactory, ProductFactory
 from order.factories import UserFactory
@@ -10,19 +11,18 @@ from product.models import Product
 
 
 class TestProductViewSet(APITestCase):
-    client = APIClient()
 
     def setUp(self):
         self.user = UserFactory()
-
         self.product = ProductFactory(
             title="pro controller",
             price=200.00,
         )
 
     def test_get_all_product(self):
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         response = self.client.get(reverse("product-list", kwargs={"version": "v1"}))
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         product_data = json.loads(response.content)
 
@@ -31,6 +31,8 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(product_data["results"][0]["active"], self.product.active)
 
     def test_create_product(self):
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
         category = CategoryFactory()
         data = json.dumps({"title": "notebook", "price": 800.00, "categories_id": [category.id]})
 
